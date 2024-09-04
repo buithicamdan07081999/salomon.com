21513f7770ade453fa55a74f4a6654cec6c91840
4dX@#82L <btcdan789@gmail.com>
2024-09-04 08:34:47 +0700

    done loaisanpham

--- backend/layouts/partials/footer.php
+++ backend/layouts/partials/footer.php
@@ -1,3 +1,6 @@
-<div>
-    Bản quyền bởi BuiDan99 &copy; 2024
+<div style="text-align: center;">
+    Bản quyền bởi BuiDan99 &copy; From <a style="color: blue;">01/06/2024</a> to
+    <a style="color: blue;"><?php 
+        echo date("d/m/Y"); // Xuất ra ngày tháng năm hiện tại
+    ?></a>
 </div>
