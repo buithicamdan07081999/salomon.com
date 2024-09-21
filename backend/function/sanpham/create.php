021e913cbd9084c83fefc9f7397b669824389a63
4dX@#82L <btcdan789@gmail.com>
2024-09-21 16:28:21 +0700

    oke

--- backend/function/sanpham/create.php
+++ backend/function/sanpham/create.php
@@ -31,7 +31,7 @@
                 // sql
                 $sql_lsp = "SELECT A.lsp_ma, A.lsp_ten FROM loaisanpham A";
                 $sql_npp = "SELECT A.npp_ma, A.npp_ten FROM nhaphanphoi A";
-                $sql_km = "SELECT A.km_ma, A.km_ten FROM khuyenmai A";
+                $sql_km = "SELECT A.km_ma, A.km_ten, A.km_noidung, A.km_tungay, A.km_denngay FROM khuyenmai A";
                 $sql_th = "SELECT A.th_ma, A.th_ten FROM thuonghieu A";
                 // thực thi
                 $data_lsp = mysqli_query($conn, $sql_lsp);
@@ -60,6 +60,9 @@
                     $arrDs_km[] = array(
                         'km_ma' => $row['km_ma'],
                         'km_ten' => $row['km_ten'],
+                        'km_noidung' => $row['km_noidung'],
+                        'km_tungay' => $row['km_tungay'],
+                        'km_denngay' => $row['km_denngay'],
                     );
                 }
                 while ($row = mysqli_fetch_array($data_th, MYSQLI_ASSOC)) {
@@ -90,20 +93,27 @@
                             <div class="mb-3">
                                 <label class="form-label">Khuyến mãi:</label>
                                 <select name="km_ma" id="km_ma" class="form-select">
+                                    <option value="">Chọn hình thức khuyến mãi</option>
+                                    <!-- option: Lý do là có thể chọn khuyến mãi hoặc không -->
                                     <?php foreach ($arrDs_km as $km): ?>
-                                        <option value="<?= $km['km_ma'] ?>"><?= $km['km_ten'] ?></option>
+                                        <option value="<?= $km['km_ma'] ?>">
+                                            <?= $km['km_ten'] ?>
+                                            |
+                                            <?= $km['km_noidung'] ?>
+                                            ( <?= date('d/m/Y', strtotime($km['km_tungay'])) ?> - <?= date('d/m/Y', strtotime($km['km_denngay'])) ?>)
+                                        </option>
                                     <?php endforeach; ?>
                                 </select>
                             </div>
                             <div class="mb-3">
-                                <label class="form-label">Mô tả ngắn:</label>
-                                <input type="text" name="sp_mota_ngan" id="sp_mota_ngan" class="form-control">
+                                <label class="form-label">Giá cũ:</label>
+                                <input type="number" name="sp_giacu" id="sp_giacu" class="form-control" />
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label class="form-label">Số lượng sản phẩm:</label>
-                                <input type="text" name="sp_soluong" id="sp_soluong" class="form-control">
+                                <input type="number" name="sp_soluong" id="sp_soluong" class="form-control">
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Nhà phân phối:</label>
@@ -122,9 +132,16 @@
                                 </select>
                             </div>
                             <div class="mb-3">
-                                <label class="form-label">Giá sản phẩm:</label>
+                                <label class="form-label">Giá mới:</label>
                                 <input type="number" name="sp_gia" id="sp_gia" class="form-control" />
                             </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-12">
+                            <label class="form-label">Mô tả ngắn:</label>
+                            <input type="text" name="sp_mota" id="sp_mota" class="form-control" />
+
                         </div>
                     </div>
                     <div class="row">
@@ -139,35 +156,33 @@
                 <?php
                 // Nếu người dùng có bấm nút Lưu -> thì mới xử lý
                 if (isset($_POST['btnLuu'])) {
+                    date_default_timezone_get('Asia/Ho_Chi_Minh');
                     // 1. Mở kết nối
                     // include_once __DIR__ . '/../../../dbconnect.php';
                     // 2. Chuẩn bị câu lệnh
                     $sp_ten = $_POST['sp_ten'];
-                    $sp_lsp = $_POST['sp_lsp'];
-                    $sp_km = $_POST['sp_km'];
+                    $lsp_ma = $_POST['lsp_ma'];
+                    $km_ma = empty($_POST['km_ma']) ? NULL : $_POST['km_ma']; // nếu Km ko chọn thì để null 
                     $sp_mota_ngan = $_POST['sp_mota_ngan'];
                     $sp_mota_chitiet = $_POST['sp_mota_chitiet'];
 
                     $sp_soluong = $_POST['sp_soluong'];
-                    $sp_npp = $_POST['sp_npp'];
-                    $sp_th = $_POST['sp_th'];
+                    $npp_ma = $_POST['npp_ma'];
+                    $th_ma = $_POST['th_ma'];
                     $sp_gia = $_POST['sp_gia'];
+                    $sp_giacu = empty($_POST['sp_giacu']) ? NULL : $_POST['sp_giacu'];
+                    $sp_ngaycapnhat = date('Y-m-d H:i:s'); // đưa vào dữ liệu thô trong Database
 
                     if ($sp_ten != "" && $sp_lsp != "" && $sp_km != "" && $sp_mota_ngan != "" && $sp_mota_chitiet != "" && $sp_soluong != "" && $sp_npp != "" && $sp_th != "" && $sp_gia != "") {
                         $sql =
-                            "INSERT INTO sanpham (sp_ten, sp_gia, sp_mota_ngan, sp_mota_chitiet, sp_soluong )
-                VALUES ('$sp_ten', '$sp_gia', '$sp_mota_ngan', '$sp_mota_chitiet', '$sp_soluong');
-                
-                UPDATE sanpham
-                SET 
-                    sp_giacu = IF(sp_giacu IS NULL, NULL, sp_gia),  -- Nếu sp_giacu đã có giá trị thì cập nhật nó bằng giá hiện tại
-                    sp_gia = '$sp_gia'  -- Cập nhật giá mới vào cột sp_gia
-                WHERE sp_ten = '$sp_ten';  -- Điều kiện để xác định đúng sản phẩm
+                            "INSERT INTO sanpham
+                            (sp_ten, sp_gia, sp_giacu, sp_mota_ngan, sp_mota_chitiet, sp_ngaycapnhat, sp_soluong, lsp_ma, km_ma, th_ma, npp_ma)
+                            VALUES ('$sp_ten', $sp_gia, $sp_giacu, '$sp_mota_ngan', '$sp_mota_chitiet', '$sp_ngaycapnhat', $sp_soluong, '$lsp_ma',  '$km_ma', '$th_ma', '$npp_ma');
+
                 ";
                         // 3. Thực thi câu lệnh
                         mysqli_query($conn, $sql);
                         echo '<script> location.href="index.php"</script>';
-                        //var_dump($sql);
                     } else {
                         echo '<script>alert("Dữ liệu không được rỗng!");</script>';
                     }
