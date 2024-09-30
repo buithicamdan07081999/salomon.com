748917e9f7e2f83a2810b5fbc2eaa4f6affcb261
BUIDAN <btcdan789@gmail.com>
2024-09-30 23:26:44 +0700

    sai chỗ vender

--- backend/layouts/partials/sidebar.php
+++ backend/layouts/partials/sidebar.php
@@ -1,13 +1,13 @@
 <h3>Chức năng</h3>
 <div class="list-group">
-  <a href="/../salomon.com/backend/function/thuonghieu/index.php" class="list-group-item list-group-item-action active" aria-current="true">Thương hiệu</a>
-  <a href="/../salomon.com/backend/function/nhaphanphoi/index.php" class="list-group-item list-group-item-action">Nhà phân phối</a>
-  <a href="/../salomon.com/backend/function/loaisanpham/index.php" class="list-group-item list-group-item-action">Loại sản phẩm</a>
-  <a href="/../salomon.com/backend/function/sanpham/index.php" class="list-group-item list-group-item-action">Sản phẩm</a>
-  <a href="/../salomon.com/backend/function/khuyenmai/index.php" class="list-group-item list-group-item-action">Khuyến mãi</a>
-  <a href="/../salomon.com/backend/function/khachhang/index.php" class="list-group-item list-group-item-action">Khách hàng</a>
-  <a href="/../salomon.com/backend/function/hinhthucthanhtoan/index.php" class="list-group-item list-group-item-action">Hình thức thanh toán</a>
-  <a href="/../salomon.com/backend/function/hinhsanpham/index.php" class="list-group-item list-group-item-action">Hình sản phẩm</a>
+  <a href="/salomon.com/backend/function/thuonghieu/index.php" class="list-group-item list-group-item-action active" aria-current="true">Thương hiệu</a>
+  <a href="/salomon.com/backend/function/nhaphanphoi/index.php" class="list-group-item list-group-item-action">Nhà phân phối</a>
+  <a href="/salomon.com/backend/function/loaisanpham/index.php" class="list-group-item list-group-item-action">Loại sản phẩm</a>
+  <a href="/salomon.com/backend/function/sanpham/index.php" class="list-group-item list-group-item-action">Sản phẩm</a>
+  <a href="/salomon.com/backend/function/khuyenmai/index.php" class="list-group-item list-group-item-action">Khuyến mãi</a>
+  <a href="/salomon.com/backend/function/khachhang/index.php" class="list-group-item list-group-item-action">Khách hàng</a>
+  <a href="/salomon.com/backend/function/hinhthucthanhtoan/index.php" class="list-group-item list-group-item-action">Hình thức thanh toán</a>
+  <a href="/salomon.com/backend/function/hinhsanpham/index.php" class="list-group-item list-group-item-action">Hình sản phẩm</a>
   <!-- chưa code -->
   <a href="#" class="list-group-item list-group-item-action">Đơn đặt hàng</a>
 
