a9056ca9dcf9f20ca5098007c5d33f1c6db5457d
4dX@#82L <btcdan789@gmail.com>
2024-12-13 09:40:49 +0700

    print pending

--- backend/function/dondathang/print.php
+++ backend/function/dondathang/print.php
@@ -24,7 +24,7 @@
                 ?>
             </div>
             <div class="col-9">
-                <h3>ĐƠN ĐẶT HÀNG</h3><a href="../../../index.php" class="btn btn-outline-info mb-3">Trang chủ <i class="fa-solid fa-house"></i></a>
+                <h3>Trang In</h3><a href="../../../index.php" class="btn btn-outline-info mb-3">Trang chủ <i class="fa-solid fa-house"></i></a>
                 <a href="create.php" class="btn btn-primary mb-3">Thêm mới <i class="fa-solid fa-plus"></i></a>
                 <table class="table table-hover table-bordered">
                     <tr>
@@ -51,6 +51,9 @@
                             <td><?= $ddh['httt_ten'] ?></td>
                             <td class="text-end"><?= number_format($ddh['tongthanhtien'], 0 , '.', ',') ?></td>
                             <!-- <td><span class="badge text-bg-primary"><?= $ddh['tttt_ten'] ?></span></td> -->
+
+                            <td class="text-end"><?= number_format($ddh['tongthanhtien'], 0, '.', ',') ?></td>
+                            <!-- <td><span class="badge text-bg-primary"><?= $ddh['tttt_ten'] ?></span></td> -->
                             <td>
                                 <?php if($ddh['tttt_ma'] == 1): ?>
                                    <span class="badge text-bg-info">Đã đặt hàng</span>
@@ -74,7 +77,7 @@
                                     <a href="edit.php?dh_ma=<?= $ddh['dh_ma'] ?>" class="btn btn-warning">Sửa <i class="fa-regular fa-pen-to-square"></i></a>
                                     <a href="delete.php?dh_ma=<?= $ddh['dh_ma'] ?>" class="btn btn-danger">Xóa <i class="fa-regular fa-trash-can"></i></a>
                                     <?php else: ?>
-                                        <a href="print.php" class="btn btn-success">In<i class="fa-regular fa-pen-to-square"></i></a>
+                                        <a href="print.php" class="btn btn-primary">In<i class="fa-regular fa-pen-to-square"></i></a>
                                 <?php endif;?>
                                 <!-- gửi bằng đường GET -->
 
