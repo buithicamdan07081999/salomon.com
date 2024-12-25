832710d001eb27dbbbceebef7579093e20e557ce
4dX@#82L <btcdan789@gmail.com>
2024-12-25 11:21:07 +0700

    giao dien

--- layouts/partials/styles.php
+++ layouts/partials/styles.php
@@ -1,6 +1,23 @@
 <link href="/salomon.com/vendor/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 <link href="/salomon.com/vendor/fontawesome/js/all.min.js" type="text/css" rel="stylesheet">
 <style>
+    .clear-fix {
+        clear: both;
+    }
+
+    body {
+        font-size: 0.8rem;
+    }
+
+    body * {
+        box-sizing: border-box;
+        /* Đảm bảo đúng kích thước */
+        margin: 0px;
+        /* reset hết giá trị mặc định */
+        padding: 0xp;
+        /* reset hết giá trị mặc định */
+    }
+
     .hinh-sanpham {
         width: 120px;
         height: 120px;
@@ -35,4 +52,84 @@
         text-align: right;
         margin-right: 5px;
     }
+
+    #topbar {
+        border-bottom: 1px solid green;
+        padding: 19px;
+    }
+
+    #topbar-left,
+    #topbar-right {
+        float: left;
+        width: 30%;
+    }
+
+    #topbar-middle {
+        float: left;
+        width: 40%;
+    }
+
+    #topbar-middle p {
+        text-align: center;
+    }
+
+    .topbar-left-list,
+    .topbar-right-list {
+        margin: 0px;
+        padding-left: 2px;
+
+    }
+
+    #topbar-right,
+    .topbar-right-list {
+        float: right;
+    }
+
+
+    .topbar-left-list li,
+    .topbar-right-list li {
+        list-style-type: none;
+        /* bỏ dấu chấm tròn */
+        padding-right: 2px;
+    }
+
+    .topbar-right-list li {
+        float: right;
+        border-left: 1px solid royalblue;
+    }
+
+
+    .topbar-left-list li {
+        float: left;
+        border-right: 1px solid royalblue;
+    }
+
+
+    .topbar-left-list li a,
+    .topbar-right-list li a {
+        list-style-type: none;
+        /* bỏ dấu chấm tròn */
+        float: left;
+        text-decoration: none;
+        /* bỏ dấu gạch chân */
+        padding: 5px;
+        color: royalblue;
+    }
+
+    .topbar-left-list li:last-child {
+        /* background-color: red; test */
+        border-right: unset;
+        /* không kẻ đường gạch */
+    }
+
+    .topbar-right-list li:last-child {
+        /* background-color: red; test */
+        border-left: unset;
+        /* không kẻ đường gạch */
+    }
+
+    /* Debug khung */
+    /* #topbar div {
+        border: 1px solid red;
+    } */
 </style>
