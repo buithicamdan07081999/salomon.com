e69f849b5bcf8178032d53c4781cafd354922256
4dX@#82L <btcdan789@gmail.com>
2025-01-02 13:08:41 +0700

    dont thống kê

--- backend/function/pages/dashboard.php
+++ backend/function/pages/dashboard.php
@@ -40,97 +40,86 @@ if (session_id() === '') {
       </div>
     </div>
     <div class="row">
-    <main role="main" class="col-md-10 ml-sm-auto px-4 mb-2">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Bảng tin DASHBOARD</h1>
-          </div>
+      <main role="main" class="col-md-10 ml-sm-auto px-4 mb-2">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Bảng tin DASHBOARD</h1>
+        </div>
 
-          <!-- Block content -->
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-primary mb-2">
-                  <div class="card-body pb-0">
-                    <div class="text-value" id="baocaoSanPham_SoLuong">
-                      <h1>0</h1>
-                    </div>
-                    <div>Tổng số mặt hàng</div>
+        <!-- Block content -->
+        <div class="container-fluid" style="align-items: center; width:200%;">
+          <div class="row" >
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-primary mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocao_sanpham">
+                    <h1>0</h1>
                   </div>
+                  <div>Tổng số mặt hàng</div>
                 </div>
-                <button class="btn btn-primary btn-sm form-control" id="refreshBaoCaoSanPham">Refresh dữ liệu</button>
-              </div> <!-- Tổng số mặt hàng -->
-              <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-success mb-2">
-                  <div class="card-body pb-0">
-                    <div class="text-value" id="baocaoKhachHang_SoLuong">
-                      <h1>0</h1>
-                    </div>
-                    <div>Tổng số khách hàng</div>
+              </div>
+              <button class="btn btn-primary btn-sm form-control" id="refreshBaoCaoSanPham">Refresh dữ liệu</button>
+            </div> <!-- Tổng số mặt hàng -->
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-success mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocao_khachhang">
+                    <h1>0</h1>
                   </div>
+                  <div>Tổng số khách hàng</div>
                 </div>
-                <button class="btn btn-success btn-sm form-control" id="refreshBaoCaoKhachHang">Refresh dữ liệu</button>
-              </div> <!-- Tổng số khách hàng -->
-              <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-warning mb-2">
-                  <div class="card-body pb-0">
-                    <div class="text-value" id="baocaoDonHang_SoLuong">
-                      <h1>0</h1>
-                    </div>
-                    <div>Tổng số đơn hàng</div>
+              </div>
+              <button class="btn btn-success btn-sm form-control" id="refreshBaoCaoKhachHang">Refresh dữ liệu</button>
+            </div> <!-- Tổng số khách hàng -->
+            <div class="col-sm-6 col-lg-3">
+              <div class="card text-white bg-warning mb-2">
+                <div class="card-body pb-0">
+                  <div class="text-value" id="baocao_dondathang">
+                    <h1>0</h1>
                   </div>
+                  <div>Tổng số đơn hàng</div>
                 </div>
-                <button class="btn btn-warning btn-sm form-control" id="refreshBaoCaoDonHang">Refresh dữ liệu</button>
-              </div> <!-- Tổng số đơn hàng -->
-              <div class="col-sm-6 col-lg-3">
-                <div class="card text-white bg-danger mb-2">
-                  <div class="card-body pb-0">
-                    <div class="text-value" id="baocaoGopY_SoLuong">
-                      <h1>0</h1>
-                    </div>
-                    <div>Tổng số góp ý</div>
-                  </div>
-                </div>
-                <button class="btn btn-danger btn-sm form-control" id="refreshBaoCaoGopY">Refresh dữ liệu</button>
-              </div> <!-- Tổng số góp ý -->
-              <div id="ketqua"></div>
-            </div><!-- row -->
-            <div class="row">
-              <!-- Biểu đồ thống kê loại sản phẩm -->
-              <div class="col-sm-6 col-lg-6">
-                <canvas id="chartOfobjChartThongKeLoaiSanPham"></canvas>
-                <button class="btn btn-outline-primary btn-sm form-control" id="refreshThongKeLoaiSanPham">Refresh dữ liệu</button>
-              </div><!-- col -->
+              </div>
+              <button class="btn btn-warning btn-sm form-control" id="refreshBaoCaoDonHang">Refresh dữ liệu</button>
+            </div> <!-- Tổng số đơn hàng -->
+            <div id="ketqua"></div>
+          </div><!-- row -->
+          <div class="row">
+            <!-- Biểu đồ thống kê loại sản phẩm -->
+            <div class="col-sm-6 col-lg-6">
+              <canvas id="chartOfobjChartThongKeLoaiSanPham"></canvas>
+              <button class="btn btn-outline-primary btn-sm form-control" id="refreshThongKeLoaiSanPham">Refresh dữ liệu</button>
+            </div><!-- col -->
 
-            </div><!-- row -->
-          </div>
+          </div><!-- row -->
+        </div>
           <!-- End block content -->
-        </main>
+      </main>
     </div>
   </div>
 
   <!-- footer -->
-  <?php include_once(__DIR__ . '/../layouts/partials/footer.php'); ?>
+  <?php
+  include_once __DIR__ . '/../../../layouts/footer.php';
+  include_once __DIR__ . '/../../../layouts/script.php';
+  ?>
   <!-- end footer -->
-
-  <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
-  <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
 
   <!-- Các file Javascript sử dụng riêng cho trang này, liên kết tại đây -->
   <!-- Liên kết thư viện ChartJS -->
-  <script src="/php/myhand/assets/vendor/Chart.js/Chart.min.js"></script>
+  <script src="/kdbd.com/vendor/Chart.js/Chart.min.js"></script>
   <script>
     $(document).ready(function() {
       // ----------------- Tổng số mặt hàng --------------------------
       function getDuLieuBaoCaoTongSoMatHang() {
-        $.ajax('/php/myhand/backend/api/baocao-tongsomathang.php', {
+        $.ajax('/kdbd.com/backend/api/baocao_sanpham.php', {
           success: function(data) {
             var dataObj = JSON.parse(data);
             var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
-            $('#baocaoSanPham_SoLuong').html(htmlString);
+            $('#baocao_sanpham').html(htmlString);
           },
           error: function() {
             var htmlString = `<h1>Không thể xử lý</h1>`;
-            $('#baocaoSanPham_SoLuong').html(htmlString);
+            $('#baocao_sanpham').html(htmlString);
           }
         });
       }
@@ -141,15 +130,15 @@ if (session_id() === '') {
 
       // ----------------- Tổng số khách hàng --------------------------
       function getDuLieuBaoCaoTongSoKhachHang() {
-        $.ajax('/php/myhand/backend/api/baocao-tongsokhachhang.php', {
+        $.ajax('/kdbd.com/backend/api/baocao_khachhang.php', {
           success: function(data) {
             var dataObj = JSON.parse(data);
             var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
-            $('#baocaoKhachHang_SoLuong').html(htmlString);
+            $('#baocao_khachhang').html(htmlString);
           },
           error: function() {
             var htmlString = `<h1>Không thể xử lý</h1>`;
-            $('#baocaoKhachHang_SoLuong').html(htmlString);
+            $('#baocao_khachhang').html(htmlString);
           }
         });
       }
@@ -160,40 +149,21 @@ if (session_id() === '') {
 
       // ----------------- Tổng số đơn hàng --------------------------
       function getDuLieuBaoCaoTongSoDonHang() {
-        $.ajax('/php/myhand/backend/api/baocao-tongsodonhang.php', {
+        $.ajax('/kdbd.com/backend/api/baocao_dondathang.php', {
           success: function(data) {
             var dataObj = JSON.parse(data);
             var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
-            $('#baocaoDonHang_SoLuong').html(htmlString);
+            $('#baocao_dondathang').html(htmlString);
           },
           error: function() {
             var htmlString = `<h1>Không thể xử lý</h1>`;
-            $('#baocaoDonHang_SoLuong').html(htmlString);
+            $('#baocao_dondathang').html(htmlString);
           }
         });
       }
       $('#refreshBaoCaoDonHang').click(function(event) {
         event.preventDefault();
         getDuLieuBaoCaoTongSoDonHang();
-      });
-
-      // ----------------- Tổng số Góp ý --------------------------
-      function getDuLieuBaoCaoTongSoGopY() {
-        $.ajax('/php/myhand/backend/api/baocao-tongsogopy.php', {
-          success: function(data) {
-            var dataObj = JSON.parse(data);
-            var htmlString = `<h1>${dataObj.SoLuong}</h1>`;
-            $('#baocaoGopY_SoLuong').html(htmlString);
-          },
-          error: function() {
-            var htmlString = `<h1>Không thể xử lý</h1>`;
-            $('#baocaoGopY_SoLuong').html(htmlString);
-          }
-        });
-      }
-      $('#refreshBaoCaoGopY').click(function(event) {
-        event.preventDefault();
-        getDuLieuBaoCaoTongSoGopY();
       });
 
 
@@ -205,7 +175,7 @@ if (session_id() === '') {
 
       function renderChartThongKeLoaiSanPham() {
         $.ajax({
-          url: '/php/myhand/backend/api/baocao-thongkeloaisanpham.php',
+          url: '/kdbd.com/backend/api/baocao_loaisanpham.php',
           type: "GET",
           success: function(response) {
             var data = JSON.parse(response);
@@ -256,7 +226,6 @@ if (session_id() === '') {
       getDuLieuBaoCaoTongSoMatHang();
       getDuLieuBaoCaoTongSoKhachHang();
       getDuLieuBaoCaoTongSoDonHang();
-      getDuLieuBaoCaoTongSoGopY();
       renderChartThongKeLoaiSanPham();
 
     });
